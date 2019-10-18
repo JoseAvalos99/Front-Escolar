1aaab8c65fa3f4e5450e7bd9a2a3a477bd309718
Jose Avalos <jose.avalos.cbtis37@gmail.com>
2019-10-17 17:39:12 -0700

    Inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { ReactiveFormsModule } from "@angular/forms";
+import { UserComponent } from './Components/WebApp/user/user.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
