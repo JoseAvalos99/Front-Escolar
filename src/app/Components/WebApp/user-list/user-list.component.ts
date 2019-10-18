7c33aea5172f3e183c67aa9c17d60df2a38435c1
Jose Avalos <jose.avalos.cbtis37@gmail.com>
2019-10-17 19:13:05 -0700

    List in console

--- src/app/Components/WebApp/user-list/user-list.component.ts
+++ src/app/Components/WebApp/user-list/user-list.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/Services/user.service';
+import { Observable } from 'rxjs';
+import { IUser } from 'src/app/Model/user.interface';
 
 @Component({
-  selector: 'app-user-list',
-  templateUrl: './user-list.component.html',
-  styleUrls: ['./user-list.component.css']
+	selector: 'app-user-list',
+	templateUrl: './user-list.component.html',
+	styleUrls: ['./user-list.component.css']
 })
 export class UserListComponent implements OnInit {
 
-  constructor() { }
+	public userList = [];
 
-  ngOnInit() {
-  }
+	constructor(private _userService: UserService) { }
+
+	ngOnInit() {
+		this._userService.getUsers().subscribe(data => console.log(data));
+	}
 
 }
