efc5fa02b52a4a489c9d7ef3ce34157c6bca2e78
JossJoestar <jose.avalos.cbtis37@gmail.com>
2019-10-29 09:02:39 -0700

    Cambios innecesarios

--- src/app/Components/WebApp/user-child/user-child.component.ts
+++ src/app/Components/WebApp/user-child/user-child.component.ts
@@ -1,16 +1,95 @@
-import { Component, OnInit, Input, Output } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, SimpleChanges } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { IUser } from 'src/app/Model/user.interface';
+import { UserService } from 'src/app/Services/user.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
-  selector: 'app-user-child',
-  templateUrl: './user-child.component.html',
-  styleUrls: ['./user-child.component.css']
+	selector: 'app-user-child',
+	templateUrl: './user-child.component.html',
+	styleUrls: ['./user-child.component.css']
 })
 export class UserChildComponent implements OnInit {
-  @Input('name') txtName:string;
-  
-  constructor() { }
+	@Input('data') UserFormFather: FormGroup;
+	@Output() OnSaveUser = new EventEmitter<IUser>();
+	@Input('Id') id: number;
 
-  ngOnInit() {
-  }
+	constructor(private _userService: UserService, private toastr: ToastrService) { }
 
+	ngOnInit() {
+		this.UserFormFather = this.CreateFormGroup();
+		console.log(this.id);
+	}
+	ngOnChanges(changes: SimpleChanges): void {
+		if (this.id != 0) {
+			this._userService.getUser(this.id).subscribe(
+				data => {
+					this.UserFormFather = new FormGroup({
+						id: new FormControl(data.id),
+						Name: new FormControl(data.name, [Validators.required, Validators.minLength(4)]),
+						LastName: new FormControl(data.lastName, [Validators.required]),
+						NickName: new FormControl(data.nickName, [Validators.required]),
+						YearsOld: new FormControl(data.yearsOld, [
+							Validators.required,
+							Validators.min(18),
+							Validators.max(99)
+						]),
+						Gender: new FormControl(data.gender, [Validators.required]),
+						PhoneNumber: new FormControl(data.phoneNumber, [Validators.required])
+					});
+				}, error => {
+					this.toastr.error(error.error, 'Error');
+				}
+			);
+		}
+	}
+	CreateFormGroup() {
+		return new FormGroup({
+			Name: new FormControl("", [Validators.required, Validators.minLength(4)]),
+			LastName: new FormControl("", [Validators.required]),
+			NickName: new FormControl("", [Validators.required]),
+			YearsOld: new FormControl("", [
+				Validators.required,
+				Validators.min(18),
+				Validators.max(99)
+			]),
+			Gender: new FormControl("", [Validators.required]),
+			PhoneNumber: new FormControl("", [Validators.required])
+		});
+	}
+	CreateFormGroupWithData(user: IUser) {
+		return new FormGroup({
+			Name: new FormControl(user.name, [Validators.required, Validators.minLength(4)]),
+			LastName: new FormControl(user.lastName, [Validators.required]),
+			NickName: new FormControl(user.nickName, [Validators.required]),
+			YearsOld: new FormControl(user.yearsOld, [
+				Validators.required,
+				Validators.min(18),
+				Validators.max(99)
+			]),
+			Gender: new FormControl(user.gender, [Validators.required]),
+			PhoneNumber: new FormControl(user.phoneNumber, [Validators.required])
+		});
+	}
+	onSaveform() {
+		this.OnSaveUser.emit(this.UserFormFather.value);
+	}
+	get name() {
+		return this.UserFormFather.get("Name");
+	}
+	get lastName() {
+		return this.UserFormFather.get("LastName");
+	}
+	get nickName() {
+		return this.UserFormFather.get("NickName");
+	}
+	get yearsOld() {
+		return this.UserFormFather.get("YearsOld");
+	}
+	get gender() {
+		return this.UserFormFather.get("Gender");
+	}
+	get phoneNumber() {
+		return this.UserFormFather.get("PhoneNumber");
+	}
 }
--- src/app/Components/WebApp/user-father/user-father.component.ts
+++ src/app/Components/WebApp/user-father/user-father.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { UserService } from 'src/app/Services/user.service';
+import { ToastrService } from 'ngx-toastr';
+import { IUser } from 'src/app/Model/user.interface';
 
 @Component({
 	selector: 'app-user-father',
@@ -6,12 +10,45 @@ import { Component, OnInit } from '@angular/core';
 	styleUrls: ['./user-father.component.css']
 })
 export class UserFatherComponent implements OnInit {
-
-	constructor() {
+	@Output() idChange = new EventEmitter();
+	UserForm: FormGroup;
+	filterUser = "";
+	public userList: IUser[] = [];
+	id: number = 0;
+	constructor(private _userService: UserService, private toastr: ToastrService) {
 	}
-	nameChild: string = "Hola Mundo";
 
 	ngOnInit() {
+		this._userService.getUsers().subscribe(
+			data => {
+				this.userList = data;
+			}, error => {
+				this.toastr.error(error.error, 'Error');
+			}
+		);
+	}
+	AddOrEditUser(data: IUser) {
+		if (data.id) {
+			console.log('update');
+			this._userService.updateUser(data).subscribe(
+				data => {
+					console.log(data);
+					location.reload();
+				},
+				error => console.log()
+			)
+		} else {
+			console.log('nuevo');
+			this._userService.addUser(data).subscribe(
+				data => {
+					console.log(data);
+					location.reload();
+				}, error => console.log(error)
+			)
+		}
+	}
+	editUser(Id: number) {
+		this.id = Id;
 	}
 
 }
